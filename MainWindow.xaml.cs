efd4c479a5f4fa417427bdb6a092bbfd00bd623b
stigibalint <72031994+stigibalint@users.noreply.github.com>
2023-03-09 16:12:34 +0100

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+namespace WpfApp10
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            sliJegy.Value = 3;
+            dpDatum.SelectedDate = DateTime.Now;
+        }
+
+        private void sliJegy_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            lblValtozo.Content = Math.Round(sliJegy.Value);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            StreamWriter fjl = new StreamWriter("naplo.csv", true);
+            fjl.WriteLine(txtNev.Text.ToString() + ";" + cboTantargy.Text + ";", dpDatum.Text.ToString() + ";" + sliJegy.Value.ToString());
+        }
+
+        private void btnOpen_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
